943677ddb97a4794346b742104132d1268594dc2
Daniil Dotsev <daniil.dotsev@scalefocus.com>
2019-01-25 16:14:04 +0200

    Fix multiple paragraph widgets added on every change

--- src/js/decorations/paragraph.js
+++ src/js/decorations/paragraph.js
@@ -1,13 +1,21 @@
 import createDeco from '../utils/create-deco';
 
+const par = 'par';
+
 export default (
   predicate = node => node.type === node.type.schema.nodes.paragraph
 ) => (from, to, doc, decos) => {
   let newDecos = decos;
   doc.nodesBetween(from, to, (node, pos) => {
-    newDecos = predicate(node)
-      ? newDecos.add(doc, [createDeco(pos + node.nodeSize - 1, 'par')])
-      : newDecos;
+    if (predicate(node)) {
+      const decoPos = pos + node.nodeSize - 1;
+      const oldDecos = newDecos.find(
+        decoPos,
+        decoPos,
+        spec => spec.key === par
+      );
+      newDecos = newDecos.remove(oldDecos).add(doc, [createDeco(decoPos, par)]);
+    }
   });
   return newDecos;
 };
