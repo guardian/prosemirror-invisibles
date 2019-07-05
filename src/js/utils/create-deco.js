0f5fb4efe5ed54d060e3dcc443a25e28d6da756d
Daniil Dotsev <dddotsev@gmail.com>
2019-07-05 12:36:47 +0300

    Split `Element.classList.add` into 2 call to work in IE

--- src/js/utils/create-deco.js
+++ src/js/utils/create-deco.js
@@ -2,7 +2,8 @@ import { Decoration } from 'prosemirror-view';
 
 export default (pos, type) => {
   const span = document.createElement('span');
-  span.classList.add('invisible', `invisible--${type}`);
+  span.classList.add('invisible');
+  span.classList.add(`invisible--${type}`);
   return Decoration.widget(pos, span, {
     marks: [],
     key: type
