b63e816a275f4d49b2716f80cc9538e6ced60685
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2020-12-28 20:57:33 +0000

    Update rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,12 @@
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
-import eslint from "rollup-plugin-eslint";
+import { eslint } from "rollup-plugin-eslint";
 import scss from "rollup-plugin-scss";
-import babel from "rollup-plugin-babel";
+import typescript from "@rollup/plugin-typescript"
 
 export default [
   {
-    input: "src/js/index.js",
+    input: "src/js/index.ts",
     output: {
       file: "dist/invisibles.js",
       format: "cjs"
@@ -18,12 +18,12 @@ export default [
       eslint({
         exclude: ["node_modules/**"]
       }),
-      babel()
+      typescript()
     ]
   },
   {
     // Github pages
-    input: "pages/index.js",
+    input: "pages/index.ts",
     output: {
       file: "pages/dist/bundle.js",
       format: "iife",
@@ -37,7 +37,8 @@ export default [
         exclude: ["node_modules/**"]
       }),
       resolve({ browser: true }),
-      commonjs()
+      typescript(),
+      commonjs(),
     ]
   }
 ];
