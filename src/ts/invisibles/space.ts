316174251a5b7e98e531399a245d14cfb1d3a3ee
Demetrios Skamiotis <demetrios.skamiotis@guardian.co.uk>
2022-09-26 15:29:36 +0100

    inital refactor of space function

--- src/ts/invisibles/space.ts
+++ src/ts/invisibles/space.ts
@@ -1,5 +1,9 @@
 import { default as character } from "./character";
 import AddDecorationsForInvisible from "../utils/invisible";
 
-export default (predicate = (char: string) => char === " "): AddDecorationsForInvisible =>
-  character("space")(predicate);
+// The defaultPredicate may well be redundant in these functions.
+// We may not indeed need a default value as each function discretely deals with their specific node/character
+type PredicateFunction = (char: string) => boolean;
+const defaultPredicate: PredicateFunction = (char: string) => char === " ";
+const space = (predicate: PredicateFunction = defaultPredicate): AddDecorationsForInvisible => character("space")(predicate);
+export { space as default }
