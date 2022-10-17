7fde89e12e6b39cc67995af2c8879916f672465b
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2022-10-17 08:45:45 +0100

    Add builders for test
    And a test for an edge case where moving selections would prevent node invisibles from appearing

--- src/ts/__tests__/helpers.ts
+++ src/ts/__tests__/helpers.ts
@@ -5,23 +5,16 @@ import {
   paragraph,
   hardBreak,
 } from "index";
-import { addListNodes } from "prosemirror-schema-list";
-import { DOMParser, Schema } from "prosemirror-model";
-import { schema } from "prosemirror-schema-basic";
+import { doc, schema } from "prosemirror-test-builder";
 import { EditorState } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
 
-const testSchema = new Schema({
-  nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
-  marks: schema.spec.marks,
-});
-
-export const createEditor = (htmlDoc = "", isActive?: boolean): EditorView => {
+export const createEditor = (docNode = doc(), isActive?: boolean): EditorView => {
   const contentElement = document.createElement("content");
-  contentElement.innerHTML = htmlDoc;
   return new EditorView(contentElement, {
     state: EditorState.create({
-      doc: DOMParser.fromSchema(testSchema).parse(contentElement),
+      doc: docNode,
+      schema,
       plugins: [
         createInvisiblesPlugin(
           [hardBreak, paragraph, space, nbSpace],
--- src/ts/__tests__/plugin.spec.ts
+++ src/ts/__tests__/plugin.spec.ts
@@ -1,9 +1,11 @@
 import { TextSelection } from "prosemirror-state";
+import { br, doc, p } from "prosemirror-test-builder";
+
 import { createEditor } from "./helpers";
 
 describe("Invisibles plugin", () => {
   it("should render character invisibles", () => {
-    const view = createEditor(`<p>1 2 3 4</p>`);
+    const view = createEditor(doc(p("1 2 3 4")));
 
     const elements = view.dom.querySelectorAll(".invisible--space");
 
@@ -11,17 +13,15 @@ describe("Invisibles plugin", () => {
   });
 
   it("should render node invisibles", () => {
-    const view = createEditor(`<p>1</p><p>2</p>`);
+    const view = createEditor(doc(p("1"), p("2")));
 
     const elements = view.dom.querySelectorAll(".invisible--par");
 
     expect(elements.length).toEqual(2);
   });
 
-  it("should not double render invisibles when changes in the document occur", () => {
-    const view = createEditor(`
-        <p>1 2 3 4</p>
-      `);
+  it("should not double render invisibles when the selection changes", () => {
+    const view = createEditor(doc(p("1 2 3 4")));
 
     view.dispatch(
       view.state.tr.setSelection(TextSelection.create(view.state.doc, 2, 9))
@@ -32,11 +32,21 @@ describe("Invisibles plugin", () => {
     expect(elements.length).toEqual(3);
   });
 
+  it("should add node and character decorations when content is added", () => {
+    const docNode = doc(p("1 2 3 4"));
+    const view = createEditor(docNode);
+
+    view.dispatch(view.state.tr.insert(docNode.nodeSize - 2, p("5, 6, 7, 8")));
+
+    const charElements = view.dom.querySelectorAll(".invisible--space");
+    expect(charElements.length).toEqual(6);
+
+    const nodeElements = view.dom.querySelectorAll(".invisible--par");
+    expect(nodeElements.length).toEqual(2);
+  });
+
   it("should add content to node invisibles when the selection includes the invisible and the next node", () => {
-    const view = createEditor(`
-      <p>1 2 3 4</p>
-      <p>5 6 7 8</p>
-    `);
+    const view = createEditor(doc(p("1 2 3 4"), p("5 6 7 8")));
     view.dispatch(
       view.state.tr.setSelection(TextSelection.create(view.state.doc, 2, 13))
     );
@@ -50,10 +60,7 @@ describe("Invisibles plugin", () => {
   });
 
   it("should remove content from node invisibles when they are no longer included in the selection", () => {
-    const view = createEditor(`
-      <p>1 2 3 4</p>
-      <p>5 6 7 8</p>
-    `);
+    const view = createEditor(doc(p("1 2 3 4"), p("5 6 7 8")));
     view.dispatch(
       view.state.tr.setSelection(TextSelection.create(view.state.doc, 2, 13))
     );
@@ -67,5 +74,21 @@ describe("Invisibles plugin", () => {
 
     expect(firstParaInvisible.textContent).toEqual("");
     expect(secondParaInvisible.textContent).toEqual("");
+  });
+
+  it("should continue to render decorations as selection passes over them", () => {
+    const docNode = doc(p("1 2 3 4", br(), br(), p("5 6 7 8")));
+    const view = createEditor(docNode);
+
+    // This collapsed selection shifts left each iteration, to
+    // catch a bug where selections caused node decos to disappear.
+    for (let curPos = docNode.nodeSize - 2; curPos > 0; curPos--) {
+      view.dispatch(
+        view.state.tr.setSelection(TextSelection.create(view.state.doc, curPos, curPos))
+      );
+
+      const elements = view.dom.querySelectorAll(".invisible--break");
+      expect(elements.length).toBe(2);
+    }
   });
 });
