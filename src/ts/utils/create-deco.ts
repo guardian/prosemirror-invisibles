292cf54923de406ccbbe66b8c27861fa552f8dff
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2022-10-19 10:50:18 +0100

    Add additional class to selected node decorations
    to let consuming code reason about selected node decorations

--- src/ts/utils/create-deco.ts
+++ src/ts/utils/create-deco.ts
@@ -14,6 +14,7 @@ export default (
     span.classList.add("invisible");
     span.classList.add(`invisible--${type}`);
     if (markAsSelected) {
+      span.classList.add("invisible--is-selected");
       const selectedMarker = document.createElement("span");
       selectedMarker.classList.add("invisible__selected-marker");
       span.appendChild(selectedMarker);
